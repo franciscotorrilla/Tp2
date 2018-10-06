58b6c6c536bb537a9a6476cf779c9e7505a319bf
Joaconte <43884898+Joaconte@users.noreply.github.com>
2018-10-06 16:22:41 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <ctime>
 #include "Vector.h"
 #include "Ahorcado.h"
 using namespace std;
 
 //revisa que la longitud sea mayor a 2.
 unsigned corroborarLongitud(unsigned longitud);
+string buscadorPalabra(unsigned longitud);
 
 int main() {
+    //randomiza la seed del random.
+    srand(time(NULL));
     unsigned longitud=0;
+    string palabra;
     cout << "Ingrese la longitud de la palabra a adivinar: ";
     cin >> longitud;
     longitud = corroborarLongitud(longitud);
-    Ahorcado ahorcado(longitud);
+    palabra = buscadorPalabra(longitud);
+    Ahorcado ahorcado(palabra);
     while (longitud!=0){
         ahorcado.juego();
         cout << "Ingrese la longitud de la palabra a adivinar (0 si quiere dejar de jugar): ";
         cin >> longitud;
         longitud = corroborarLongitud(longitud);
-        ahorcado.nuevaPalabra(longitud);
+        palabra = buscadorPalabra(longitud);
+        ahorcado.nuevaPalabra(palabra);
     }
     return 0;
 }
@@ -28,5 +36,30 @@ unsigned corroborarLongitud(unsigned longitud){
         cout << "Ingrese una longitud valida (debe ser mayor a 2): ";
         cin >> longitud;
     }
+    return longitud;
 }
 
+string buscadorPalabra(unsigned longitud){
+
+    ifstream archivo;
+    unsigned pos=0, random=0;
+    string linea, listado[2000];
+    while (pos==0){
+        archivo.open("palabras.txt");
+        while (archivo>>linea){
+            if (linea.length()==longitud){
+                listado[pos]=linea;
+                pos++;
+            }
+        }
+        archivo.close();
+        if (pos==0){
+            cout << "No tenemos palabras con esa longitud." << endl;
+            cout << "Ingrese la longitud de la palabra a adivinar: ";
+            cin >> longitud;
+            longitud = corroborarLongitud(longitud);
+        }
+    }
+    random = rand() % pos;
+    return listado[random];
+}
